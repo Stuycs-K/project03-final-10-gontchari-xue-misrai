48e65bc41d1b72eb6498d036b01b996286e657a6
Ivan Gontchar <ivan.tardis@outlook.com>
2025-01-06 15:12:16 -0500

    universal h file added

---
+++ universal.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+
+#ifndef UNIVERSAL_H
+#define UNIVERSAL_H
+#define WKP "mario"
+
+#endif
