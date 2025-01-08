e690b8bb9755748956d82d2cb668d9b3e81ad241
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-08 17:48:54 -0500

    add colors and scrolling, along with chat window typing in the chatbox

--- renderer.c
+++ renderer.c
@@ -9,34 +9,46 @@
 
 #include "renderer.h"
 
-#define MAX_INPUT 50
+#define MAX_INPUT 100
 #define MAX_CHAT 30000
 
 char input[MAX_INPUT] = {0};  // Input buffer
-char chat[MAX_CHAT] = "READY PLAYER 1";
+char chat[MAX_CHAT] = "READY PLAYER 0\n";
+int ready_player_num = 0;
 
 WINDOW *input_win, *chat_win;
 int LINES, COLS, chat_open = 1;  // Default window size
 
 int main() {
   setlocale(LC_ALL, "");
-
   initscr();  // Initialize ncurses
-  noecho();   // Disable automatic echoing of typed characters
-  cbreak();   // Disable line buffering
 
-  keypad(stdscr, TRUE);  // Enable function keys
-  getmaxyx(stdscr, LINES, COLS);
+  start_color();
+  init_pair(1, COLOR_RED, COLOR_GREEN);   // chat box
+  init_pair(2, COLOR_CYAN, COLOR_BLACK);  // prompt
+
+  noecho();  // Disable automatic echoing of typed characters
+  cbreak();  // Disable line buffering
+
   signal(SIGWINCH, handle_resize);
   signal(SIGINT, handle_sigint);
   int pos = 0;  // Current cursor position
 
+  keypad(stdscr, TRUE);  // Enable function keys
+  getmaxyx(stdscr, LINES, COLS);
+
   input_win = newwin(3, COLS, LINES - 3, 0);  // Create input window
   box(input_win, 0, 0);                       // Draw border around input window
-  chat_win = newwin(LINES - 4, COLS, 1, 0);
-  box(chat_win, 0, 0);  // Draw border around input window
 
-  mvprintw(0, 0, "Type something (Backspace to delete, Enter to submit):");
+  chat_win = newwin(LINES - 4, COLS, 1, 0);
+  scrollok(chat_win, TRUE);
+  attron(COLOR_PAIR(1));
+  box(chat_win, 0, 0);  // Draw border around input window
+  attroff(COLOR_PAIR(2));
+  char *prompt = "CHAT WINDOW";
+  attron(COLOR_PAIR(2));
+  mvprintw(0, (COLS - strlen(prompt)) / 2, prompt);
+  attroff(COLOR_PAIR(1));
   refresh();  // Refresh main screen
 
   mvwprintw(chat_win, 2, 3, "%s", chat);
@@ -55,16 +67,21 @@ int main() {
       handle_backspace(input_win, chat_win, &pos, input);
     } else if (pos < MAX_INPUT - 1 && ch >= 32 && ch <= 126) {
       handle_normal_characters(input, &pos, ch);
-    }
+      delwin(chat_win);
+      chat_win = newwin(LINES - 4, COLS, 1, 0);
+      scrollok(chat_win, TRUE);
 
-    delwin(chat_win);
-    chat_win = newwin(LINES - 4, COLS, 1, 0);
-    char *chat_add = "READY PLAYER 1";
-    box(chat_win, 0, 0);  // Draw border around input window
-    strcat(chat, chat_add);
-    mvwprintw(chat_win, 3, 3, "%s", chat);
-    wrefresh(chat_win);
-    refresh();
+      char chat_add[20];
+      sprintf(chat_add, "  READY PLAYER %d\n", ready_player_num);
+      ready_player_num++;
+      strcat(chat, chat_add);
+      mvwprintw(chat_win, 2, 2, "%s", chat);
+      attron(COLOR_PAIR(1));
+      box(chat_win, 0, 0);  // Draw border around input window
+      attroff(COLOR_PAIR(1));
+      wrefresh(chat_win);
+      refresh();
+    }
   }
 
   // Final output after exiting input
@@ -106,17 +123,25 @@ void handle_resize(int sig) {
 
     getmaxyx(stdscr, LINES, COLS);
 
-    mvprintw(0, 0, "Type something (Backspace to delete, Enter to submit):");
+    attroff(COLOR_PAIR(2));
+    char *prompt = "CHAT WINDOW";
+    attron(COLOR_PAIR(2));
+    mvprintw(0, (COLS - strlen(prompt)) / 2, prompt);
     refresh();
 
     delwin(chat_win);
     chat_win = newwin(LINES - 4, COLS, 1, 0);
+    scrollok(chat_win, TRUE);
+    mvwprintw(chat_win, 2, 3, "%s", chat);
+    attron(COLOR_PAIR(1));
     box(chat_win, 0, 0);  // Draw border around input window}
+    attroff(COLOR_PAIR(1));
     wrefresh(chat_win);
 
     delwin(input_win);
     input_win = newwin(3, COLS, LINES - 3, 0);  // Create input window
     box(input_win, 0, 0);  // Draw border around input window
+    mvwprintw(input_win, 1, 1, "%s", input);  // Display input buffer
     wrefresh(input_win);
   }
 }
