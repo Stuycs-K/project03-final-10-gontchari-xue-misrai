4e74a1d12690f3ea4059f49bd8f1f616391c47e8
ishaanam <ishaana.misra@gmail.com>
2025-01-18 17:50:38 -0500

    Revert "remove unecesssary files"
    This reverts commit 059ef6a0bedb595e389e576be920864da2512abc.

---
+++ renderer.h
@@ -0,0 +1,10 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+#include <ncurses.h>
+void handle_enter(int *chat_open);
+void handle_backspace(WINDOW *input_win, WINDOW *chat_win, int *pos,
+                      char *input);
+void handle_normal_characters(char *input, int *pos, char ch);
+void handle_resize(int sig);
+void handle_sigint(int sig);
+#endif
