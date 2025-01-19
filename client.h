7439e5e57273489e038a6f255b4a6cc1dad4f95a
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-18 20:02:07 -0500

    resolve merge

--- client.h
+++ client.h
@@ -2,12 +2,4 @@
 #define CLIENT_H
 void handle_sigint(int sig);
 void handle_resize(int sig);
-<<<<<<< HEAD
-void render_channel_box();
-void render_people_box();
-void render_input_box();
-void render_chat_box();
-void update_cursor_position();
-=======
->>>>>>> parent of 872f09d (add some helper functions)
 #endif
