8d57fec8451f456526cc691f1791d075322c9b1d
Ivan Gontchar <ivan.tardis@outlook.com>
2025-01-20 11:47:00 -0500

    trying to have the error messages working

--- client.c
+++ client.c
@@ -261,6 +261,7 @@ int main() {
             // printf(" post parse args\n");
             // Does this actually work?
             if(sizeof(args) < 2){
+              // strcat(chat, "Did not provide a channel name for a second argument.\n");
               printf("Did not provide a channel name for a second argument.\n");
             }
             else if(sizeof(args) > 2 && args[2] != NULL){
@@ -268,6 +269,7 @@ int main() {
               // printf("1: %s\n", args[1]);
               // printf("2: %s\n", args[2]);
               // printf("3: %s\n", args[3]);
+              // strcat(chat, "You must provide only 2 arguments: the command, and the channel name to be used for the command.\n");
               printf("You must provide only 2 arguments: the command, and the channel name to be used for the command.\n");
             }
 
@@ -290,6 +292,7 @@ int main() {
             else{
               // TODO: what happens here if a command is not valid
               // This is a placeholder print because I don't know the implications of putting this here
+              // strcat(chat, "That is not a valid command please use one of:\n\t/createChannel\n\t/changeChannel\n\t/closeChannel\n");
               printf("That is not a valid command please use one of:\n\t/createChannel\n\t/changeChannel\n\t/closeChannel\n");
             }
           }
